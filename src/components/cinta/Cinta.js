e5a44dc82742fb26687b2e1ad30b01fc426c2de4
LigorioSalgado <salgadolg5@hotmail.com>
2018-02-15 03:18:56 -0600

    pre produccion

--- src/components/cinta/Cinta.js
+++ src/components/cinta/Cinta.js
@@ -18,15 +18,21 @@ class Cinta extends Component {
     constructor(props){
         super(props);
 
-        this.state = {cinta:""}
+        this.state = {cinta:"",precios:""}
     }
 
 
     componentDidMount(){
-        const cities = Firebase.database().ref().child('programas').child(this.props.match.params.program);
-        cities.on('value',content => {
+        const cinta = Firebase.database().ref().child('programas').child(this.props.match.params.program);
+        cinta.on('value',content => {
             console.log(content.val())
             this.setState({cinta:content.val()})
+        });
+
+        const precios = Firebase.database().ref().child('precios');
+        precios.on('value',content => {
+            console.log(content.val())
+            this.setState({precios:content.val()})
         });
 
     }
@@ -34,27 +40,32 @@ class Cinta extends Component {
 
 
     render() {
-        console.log(this.state.cinta)
-        return (
-            <div>
-                <Contacto/>
-                <Navbar/>
-                <Discount/>
-                <CintaHead/>
-                <div className="container-fluid">
-                    <Contenido objective={this.state.cinta.objetivo}
-                               temas={this.state.cinta.temas}
-                    />
+        if(this.state.precios) {
+            return (
+                <div>
+                    <Contacto/>
+                    <Navbar/>
+                    <Discount dvalue={this.state.precios.discount.cantidad} dcode={this.state.precios.discount.cupon}/>
+                    <CintaHead programa={this.state.cinta.name}/>
+                    <div className="container-fluid">
+                        <Contenido objective={this.state.cinta.objetivo}
+                                   temas={this.state.cinta.temas}
+                                   img={this.state.cinta.img}
+                                   precios={this.state.precios}
+                        />
 
+
+                    </div>
+                    <Carousel/>
+                    <Community/>
+                    <Companies/>
+                    <Footer/>
 
                 </div>
-                <Carousel/>
-                <Community/>
-                <Companies/>
-                <Footer/>
-
-            </div>
-        );
+            );
+        }else{
+            return(<div>Cargando...</div>)
+        }
     }
 }
 
--- src/components/cinta/contenido/Contenido.js
+++ src/components/cinta/contenido/Contenido.js
@@ -12,69 +12,81 @@ class Contenido extends Component {
 
 
     render() {
+        if(!this.props.objective){
+            return (<div></div>);
+        }
+        else{
+            let  temas = this.props.temas.map((tema) =>
+                <li>
+                    {tema}
+                </li>
+            );
+            return (
+                <div>
+                    <div className="row justify-content-center pb-5" style={{backgroundColor:"#F9F9F9"}}>
+                        <div className="col-md-12 col-lg-12 text-center">
+                            <img src={this.props.img} alt="" className="rounded-circle cinta-image"/>
+                            <div className="col-md-12 col-lg-12 text-center">
+                                <h4 className="objective">Objetivos del programa</h4>
+                            </div>
+                            <div className="col-md-12 col-lg-12 text-center">
+                                <p className="objective-text">{this.props.objective}</p>
+                            </div>
 
+                            <div className="col-md-10 col-lg-10 mx-auto" style={{backgroundColor:"#FFF"}}>
+                                <div className="row justify-content-center pt-4 pb-4">
+                                    <div className="col-md-12 col-lg-12 text-center pb-3"  style={{borderBottom:"solid #F9F9F9 2px"}}>
+                                        <h5 className="objective">Contenido</h5>
+                                    </div>
+                                    <div className="col-md-12 col-lg-12 text-justify mt-3"  style={{borderBottom:"solid #F9F9F9 2px"}}>
+                                        <ul className="cinta-list">
+                                            {temas}
+                                        </ul>
+                                    </div>
+                                    <div className="col-md-12 col-lg-12 text-center pb-3 pt-3" style={{borderBottom:"solid #F9F9F9 2px"}}>
+                                        <p className="info-text">
+                                            Nuestra metodologia Hacker Path se divide en 2 etapas generales: Una primera etapa de Fundamentos teoricos
+                                            (Teoria+Practica) y la segunda etapa de Creacion de producto (100% hands on) por
+                                            lo que durante el programa, estaras expuesto a diferentes Sprints, procesos y
+                                            herramientas con las que se trabaja en la industria, mientras aprendes desarrollando
+                                            productos reales en equipo.
 
-
-        return (
-           <div>
-               <div className="row justify-content-center pb-5" style={{backgroundColor:"#F9F9F9"}}>
-                   <div className="col-md-12 col-lg-12 text-center">
-                       <img src={cinta} alt="" className="rounded-circle cinta-image"/>
-                       <div className="col-md-12 col-lg-12 text-center">
-                           <h4 className="objective">Objetivos del programa</h4>
-                       </div>
-                       <div className="col-md-12 col-lg-12 text-center">
-                           <p className="objective-text">{this.props.objective}</p>
-                       </div>
-
-                       <div className="col-md-10 col-lg-10 mx-auto" style={{backgroundColor:"#FFF"}}>
-                           <div className="row justify-content-center pt-4 pb-4">
-                               <div className="col-md-12 col-lg-12 text-center pb-3"  style={{borderBottom:"solid #F9F9F9 2px"}}>
-                                   <h5 className="objective">Contenido</h5>
-                               </div>
-                               <div className="col-md-12 col-lg-12 text-justify mt-3"  style={{borderBottom:"solid #F9F9F9 2px"}}>
-                                   <ul className="cinta-list">
-                                   </ul>
-                               </div>
-                               <div className="col-md-12 col-lg-12 text-center pb-3 pt-3" style={{borderBottom:"solid #F9F9F9 2px"}}>
-                                   <p className="info-text">
-                                       Nuestra metodologia Hacker Path se divide en 2 etapas generales: Una primera etapa de Fundamentos teoricos
-                                       (Teoria+Prac ca) y la segunda etapa de Creacion de producto (100% hands on) por
-                                       lo que durante el programa, estaras expuesto a diferentes Sprints, procesos y
-                                       herramientas con las que se trabaja en la industria, mientras aprendes desarrollando
-                                       productos reales en equipo.
-
-                                   </p>
-                               </div>
-                               <div className="col-md-12 col-lg-12 pt-4">
-                                   <p className="cinta-time">
-                                       Duración: 8 semanas | Inicia Lunes 3 de Abril de 2018 | Horario: de Lunes a Viernes de 7pm a 10pm
-                                   </p>
-                               </div>
-
-                           </div>
-                       </div>
-
-                       <div className="col-md-10 col-lg-10 mt-5 mx-auto" style={{backgroundColor:"#FFF"}}>
-                            <div className="row justify-content-center">
-                                <div className="col-md-10 col-lg-10 pt-5">
-                                    <p className="text-pago">
-                                        Con la promoción actual sólo pagas $2,100 MXN. por mes ya que ofrecemos pagar en hasta en 12 mensualidades
-                                        SIN intereses. Puedes pagar
-                                        también de contado $25,000 MXN.</p>
-                                </div>
-                                <div className="col-md-10 col-lg-10 text-center pb-5">
-                                    <a href="#" className="btn btn-buy-cinta"> INSCRÍBETE</a>
+                                        </p>
+                                    </div>
+                                    <div className="col-md-12 col-lg-12 pt-4">
+                                        <p className="cinta-time">
+                                            Duración: 5 semanas | Inicia {this.props.precios.start} | Horario: de Lunes a Viernes de 7pm a 10pm
+                                        </p>
+                                    </div>
 
                                 </div>
                             </div>
-                       </div>
+
+                            <div className="col-md-10 col-lg-10 mt-5 mx-auto" style={{backgroundColor:"#FFF"}}>
+                                <div className="row justify-content-center">
+                                    <div className="col-md-10 col-lg-10 pt-5">
+                                        <p className="text-pago">
+                                            Con la promoción actual sólo pagas ${this.props.precios.precio_month} MXN. por mes ya que ofrecemos pagar en hasta en 12 mensualidades
+                                            SIN intereses. Puedes pagar
+                                            también de contado ${this.props.precios.precio} MXN.</p>
+                                    </div>
+                                    <div className="col-md-10 col-lg-10 text-center pb-5">
+                                        <a href="#" className="btn btn-buy-cinta"> INSCRÍBETE</a>
+
+                                    </div>
+                                </div>
+                            </div>
 
 
-                   </div>
-               </div>
-           </div>
-        );
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+
+
+
     }
 }
 
