2d6833f456d32f34524d5367bbdfda37429f70cd
LigorioSalgado <salgadolg5@hotmail.com>
2018-04-21 12:57:29 -0500

    cambios en botones

--- src/components/home/cities/CityCard/CityCard.js
+++ src/components/home/cities/CityCard/CityCard.js
@@ -10,9 +10,14 @@ class CityCard extends  Component {
 
     constructor(props){
         super(props)
-        this.state = {isHovered:true};
+        this.state = {isHovered:true,div:"cdmx-roma"};
         this.onSelect = this.onSelect.bind(this);
         this.onHovered = this.onHovered.bind(this);
+    }
+
+
+    componentDidMount(){
+        document.getElementById(this.state.div).className += " active";
 
     }
 
@@ -23,7 +28,19 @@ class CityCard extends  Component {
             console.log(this.state.isHovered)
     }
 
-    onSelect(e){
+    onSelect(id){
+        console.log(id);
+        document.getElementById(id).className += " active";
+        console.log("------------------------> en try")
+        console.log(this.state.div);
+        console.log("-------------------->saliendo del try")
+        console.log(document.getElementById(this.state.div));
+        document.getElementById(this.state.div).className = document.getElementById(this.state.div).className.replace
+                ( /(?:^|\s)active(?!\S)/g , '' );
+        this.setState({div:id});
+
+        console.log("----------------> en expecion")
+
 
         this.props.changeCity(this.props.city,this.props.cityId,this.props.cityObject);
         setTimeout(function() { console.log(this.refs.cinta) }.bind(this), 1500);
@@ -33,7 +50,7 @@ class CityCard extends  Component {
         const bounce = !this.state.isHovered ? "bounce-devf" : "";
         return(
 
-            <div className="card cardStyle" onClick={this.onSelect} onMouseEnter={this.onHovered} onMouseLeave={this.onHovered} id={this.props.idDiv}>
+            <div className="card cardStyle" onClick={(e) => this.onSelect(this.props.idDiv)} onMouseEnter={this.onHovered} onMouseLeave={this.onHovered} id={this.props.idDiv}>
                 <img className="card-img-top card-photo" src={this.props.imagenUrl} alt="Card image cap"/>
                 <div className="card-img-overlay">
                     <h4>{this.props.city.split(" ")[0]}</h4>
@@ -41,7 +58,7 @@ class CityCard extends  Component {
                     <div className="card-body">
                         <p className="card-text">{this.props.city}</p>
                         <i
-                            className={bounce+" arrow-devf fa fa-angle-down"}>
+                            className={"animated infinite bounce arrow-devf fa fa-angle-down"}>
 
 
                         </i>
